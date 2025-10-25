75f9010ea40470f1d89b89051abcc599b0e0cbc4
mohamednasr5 <elfannanm@gmail.com>
2025-10-26 01:41:49 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
